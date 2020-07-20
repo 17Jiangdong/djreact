6b44984226aed5c1316388fb68084705ead3a95a
17Jiangdong <48679644+17Jiangdong@users.noreply.github.com>
2020-07-20 22:42:58 +0100

    changes

--- frontend/gui/src/components/Form.js
+++ frontend/gui/src/components/Form.js
@@ -7,54 +7,54 @@ const FormItem = Form.Item;
 
 class CustomForm extends React.Component {
 
-    handleFormSubmit = (event, requestType, articleID) => {
-        const title = event.target.elements.title.value;
-        const content = event.target.elements.content.value;
-        const description = event.target.elements.description.value;
+  handleFormSubmit = (event, requestType, articleID) => {
+    const title = event.target.elements.title.value;
+    const content = event.target.elements.content.value;
+    const description = event.target.elements.description.value;
 
-        switch ( requestType ) {
-            case 'post':
-                return axios.post('http://127.0.0.1:8000/api/', {
-                    title: title,
-                    content: content,
-                    description: description
-                })
-                .then(res => console.log(res))
-                .catch(error => console.err(error));
-            case 'put':
-                return axios.put(`http://127.0.0.1:8000/api/${articleID}/`, {
-                    title: title,
-                    content: content,
-                    description: description
-                })
-                .then(res => console.log(res))
-                .catch(error => console.err(error));
-        }
+    switch (requestType) {
+      case 'post':
+        return axios.post('http://127.0.0.1:8000/api/', {
+          title: title,
+          content: content,
+          description: description
+        })
+          .then(res => console.log(res))
+          .catch(error => console.err(error));
+      case 'put':
+        return axios.put(`http://127.0.0.1:8000/api/${articleID}/`, {
+          title: title,
+          content: content,
+          description: description
+        })
+          .then(res => console.log(res))
+          .catch(error => console.err(error));
     }
+  }
 
-    render() {
-        return (
-        <div>
-            <form onSubmit={(event) => this.handleFormSubmit(
-                event,
-                this.props.requestType,
-                this.props.articleID )}>
-            <FormItem label="Title" >
-                <Input name="title" placeholder="Put a title here" />
-            </FormItem>
-            <FormItem label="Content" >
-                <Input name="content" placeholder="Enter some content ..." />
-            </FormItem>
-            <FormItem label="Description" >
-                <Input name="description" placeholder="Enter some content ..." />
-            </FormItem>
-            <FormItem>
-                <Button type="primary" htmlType="submit">{this.props.btnText}</Button>
-            </FormItem>
-            </form>
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleFormSubmit(
+          event,
+          this.props.requestType,
+          this.props.articleID)}>
+          <FormItem label="Title" >
+            <Input name="title" placeholder="Put a title here" />
+          </FormItem>
+          <FormItem label="Content" >
+            <Input name="content" placeholder="Enter some content ..." />
+          </FormItem>
+          <FormItem label="Description" >
+            <Input name="description" placeholder="Enter some content ..." />
+          </FormItem>
+          <FormItem>
+            <Button type="primary" htmlType="submit">{this.props.btnText}</Button>
+          </FormItem>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CustomForm;
